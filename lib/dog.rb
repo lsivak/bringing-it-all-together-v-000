71b3ea0a87f8ce35c87aa6eac650cbf7b72d4e78
Lara Sanders <larasanders@gmail.com>
2017-12-01 20:06:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,7 @@ end
 def find_or_create_by(name:, breed:)
   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, AND breed = ?", name, breed)
   if !dog.empty?
-    dog_info = dogs[0]
+    dog_info = dog[0]
     dog = Dog.new(id: dog_info[0], name: dog_info[1], breed: dog_info[3])
   else
     dog = self.create(name: name, breed: breed)
